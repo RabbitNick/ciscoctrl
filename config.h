d9697562d2bb09aa3e4497286e7b224b679c2b14
Nick Chan <nick.chitchan@gmail.com>
2013-12-06 23:37:18 +0800

    This version was modified for telnet and added boost c++ library. It can compile well and communicate to server successfully.

--- config.h
+++ config.h
@@ -1,2 +1,24 @@
 
+#ifndef CONFIG_H
+#define CONFIG_H 1
+
+
 #include "./src/ciscoctrl.h"
+#include <stdlib.h>
+#include <iostream>
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/option.hpp>
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
